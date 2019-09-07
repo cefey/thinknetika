7a0896c28471c2600b8a88d33abcdc5cfd777c39
desperado321 <staffa261@gmail.com>
2019-09-07 21:55:58 +0400

    Delete

--- main.rb
+++
@@ -1,6 +0,0 @@
-
-require_relative 'rail_road.rb'
-
-new_rail_road = RailRoad.new
-new_rail_road.main_menu
-
--- wagon.rb
+++
@@ -1,9 +0,0 @@
-require_relative 'manufacturer.rb'
-class Wagon
-  include Manufacturer
-  attr_reader :type
-  def initialize(type)
-    @type = type
-  end
-end
-
