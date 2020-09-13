188c8a9e265f9a131c30e19d5d63100373fd4cdb
cefey <staffa261@gmail.com>
2020-09-13 21:36:45 +0400

    Blackjack 1.04

---
+++ blackjack/blackjack.rb
@@ -0,0 +1,11 @@
+require_relative 'message'
+require_relative 'card'
+require_relative 'deck'
+require_relative 'hand'
+require_relative 'user'
+require_relative 'dealer'
+require_relative 'player'
+require_relative 'game_io'
+require_relative 'game'
+
+Game.new
